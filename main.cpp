4208c06a663ad600ba7bbd4183071d83df5b974f
chaya <2371643332+Zzz158425@users.noreply.github.com>
2026-05-10 14:28:57 +0800

    修改为 sort 算法为快排算法

--- main.cpp
+++ main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[], int i, int j)
+{
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l < r)
+	{	
+		while(l < r && arr[l] < arr[r])
+		{
+			r--;
+		}
+		if(l < r)
+		{
+			arr[l++] = arr[r];
+		}
+		while(l < r && arr[l] > arr[r])
+		{
+			l++;
+		}
+		if(l < r)
+		{
+			arr[r--] = arr[l];
+		}
+	}
+	arr[l] = val;
+	sort(arr, i, l - 1);
+	sort(arr, l + 1, j);
+}
 void sort(int arr[], int size)
 {
-	int tmp = 0;
-	bool flag = false;
-	for(int i = 0;i < size - 1; ++i) 
-	{
-		for(int j = 0;j < size - 1; ++j) 
-		{
-			if(arr[j] > arr[j + 1])
-			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-				flag = true;
-			}
-		}
-		if(!flag)
-			break;
-	}
+	sort(arr, 0, size);
 }
 int main()
 {
@@ -27,5 +39,10 @@ int main()
 	int arr[] = { 12, 4, 89, 43, 21, 78 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
+	for(int i = 0; i < size; ++i)
+	{
+		cout << arr[i] << " ";
+		cout << endl;
+	}
 	return 0;
 }
