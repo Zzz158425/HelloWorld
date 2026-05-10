acfbd02e92a20e58af04743f9ea881af6ab68405
chaya <2371643332+Zzz158425@users.noreply.github.com>
2026-05-10 14:12:59 +0800

    冒泡排序

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void sort(int arr[], int size)
 {
 	int tmp = 0;
+	bool flag = false;
 	for(int i = 0;i < size - 1; ++i) 
 	{
 		for(int j = 0;j < size - 1; ++j) 
@@ -13,8 +14,11 @@ void sort(int arr[], int size)
 				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
+				flag = true;
 			}
 		}
+		if(!flag)
+			break;
 	}
 }
 int main()
